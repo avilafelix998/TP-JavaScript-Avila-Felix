95ae699c1abc048694431f8a448b192d075e3d8a
felix <avilafelix998@gmail.com>
2024-05-06 11:35:48 -0300

    se desarrollo el rangoEdad

--- assets/js/rangoEdad.js
+++ assets/js/rangoEdad.js
@@ -1,1 +1,13 @@
 let numero = parseInt(prompt("ingrese un su edad: "))
+if(numero <= 12){
+    console.log("usted es un niño")
+}
+else if(numero > 12 && numero < 18){
+    console.log("usted es un adolecente")
+}
+else if(numero >=18 && numero < 60){
+    console.log("usted es un adulto")
+}
+else if(numero >=60){
+    console.log("usted es un adulto mayor")
+}
