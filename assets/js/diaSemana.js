d02344edb3d30080425dc2795a5deed706d41978
felix <avilafelix998@gmail.com>
2024-05-06 10:31:16 -0300

    se desarrollo el diaSemana.js

--- assets/js/diaSemana.js
+++ assets/js/diaSemana.js
@@ -1,1 +1,25 @@
-let dia = parseInt(prompt("ingrese un dia de la semana: "))
+let dia = parseInt(prompt("ingrese un numero de la semana: "))
+if (dia == 1) {
+    console.log("lunes")
+}
+else if (dia == 2) {
+    console.log("martes")
+}
+else if (dia == 3) {
+console.log("miercoles")
+}
+else if(dia == 4){
+    console.log("jueves")
+}
+else if(dia == 5){
+    console.log("viernes")
+}
+else if(dia ==6){
+    console.log("sabado")
+}
+else if(dia ==7){
+    console.log("domingo")
+}
+else{
+    console.log("Error: numero no valido")
+}
