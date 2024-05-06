1f4be91efde056fea81b29f6e26bf893417290f1
felix <avilafelix998@gmail.com>
2024-05-06 11:47:47 -0300

    se desarrollo parImpar

---
+++ assets/js/parImpar.js
@@ -0,0 +1,9 @@
+let numero = parseInt(prompt("ingrese un numero: "))
+
+    if(numero % 2 === 0) {
+        console.log(numero + " es " + "par");
+    } else {
+        console.log(numero + " es " + "impar");
+    }
+
+
